df37931b9dabd431dfec283c20250d0862da1988
yeetem710 <126434100+yeetem710@users.noreply.github.com>
2025-05-24 02:17:00 -0400

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,12 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Paperclip Clicker Game',
   description: 'A simple clicker game about making paperclips',
+  icons: [
+    { rel: 'icon', url: '/assets/favicon.png' },
+    { rel: 'icon', url: '/favicon.ico' },
+    { rel: 'apple-touch-icon', url: '/assets/apple-touch-icon.png' },
+    { rel: 'shortcut icon', url: '/favicon.ico' },
+  ],
 };
 
 export default function RootLayout({
@@ -19,6 +25,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/assets/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/assets/apple-touch-icon.png" />
+      </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <AuthProvider>
           <Header />
